dee272a1e270d95a70e7d7243ab8e2465e6f834e
plx-edu <plx-edu@hotmail.com>
2022-03-03 11:16:52 +0100

    added prisma service (nest recommended) and small implementation on transactions

--- backend/src/transactions/transactions.service.ts
+++ backend/src/transactions/transactions.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { PrismaService } from '../prisma.service';
+import { Transaction, Prisma } from '@prisma/client';
 
 @Injectable()
 export class TransactionsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createTransactionDto: CreateTransactionDto) {
     return 'This action adds a new transaction';
   }
 
-  findAll() {
-    return `This action returns all transactions`;
+  async findAll(): Promise<Transaction[]> {
+    // return `This action returns all transactions`;
+    return this.prisma.transaction.findMany();
   }
 
   findOne(id: number) {
