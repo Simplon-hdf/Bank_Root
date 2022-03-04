644601c689263caea8f8f03d5cfde1edcdecf564
plx-edu <plx-edu@hotmail.com>
2022-03-04 18:33:16 +0100

    added basic update to account balance after transaction

--- backend/src/transactions/transactions.service.ts
+++ backend/src/transactions/transactions.service.ts
@@ -25,62 +25,51 @@ export class TransactionsService {
     // console.log(':::::\n', transaction, '\n:::::');
 
     // Add detail(s) about transaction
-    const createDetailDto: CreateDetailDto = {
-      transaction_id: transaction.transaction_id,
-      account_id: transaction.from_account_id,
-      amount: transaction.amount,
-      type: transaction.type,
-      status_code: transaction.status_code,
-    };
+    for (let i = 0; i < 2; i++) {
+      const createDetailDto: CreateDetailDto = {
+        transaction_id: transaction.transaction_id,
+        account_id:
+          i === 0 ? transaction.from_account_id : transaction.to_account_id,
+        amount: i === 0 ? -transaction.amount : transaction.amount,
+        type: transaction.type,
+        status_code: transaction.status_code,
+      };
 
-    const detail = await this.prisma.transaction_details.create({
-      data: createDetailDto,
-    });
-    // console.log(':::::\n', detail, '\n:::::');
+      const detail = await this.prisma.transaction_details.create({
+        data: createDetailDto,
+      });
+      // console.log(':::::\n', detail, '\n:::::');
+
+      // Updating account balance
+      const account = await this.prisma.account
+        .findUnique({
+          where: { account_id: detail.account_id },
+        })
+        .then((acc) => {
+          console.log('########', acc);
+          return this.prisma.account.update({
+            where: { account_id: acc.account_id },
+            data: {
+              account_balance: acc.account_balance + detail.amount,
+            },
+          });
+        });
+      // console.log(':::::\n', account, '\n:::::');
+    }
 
     return transaction;
   }
 
   async findAll(): Promise<Transaction[]> {
-    // return `This action returns all transactions`;
-
-    /*
-    const v = await this.prisma.client.create({
-      data: {
-        title: 'Mister',
-        name: 'Danny',
-        surname: 'B.',
-        email: 'danny@mail.com',
-        address: '123 Sreet Road',
-        telephone: '0123456789',
-        login: 'danny',
-        password: 'pwd',
-      },
-    });
-
-    const v2 = await this.prisma.account.create({
-      data: {
-        client_id: v.client_id,
-        account_number: 12345,
-        account_balance: 2341567,
-        status_code: false,
-      },
-    });
-    
-    console.log('here three', v, v2);
-    */
-
     return this.prisma.transaction.findMany();
   }
 
   async findOne(id: number): Promise<Transaction> {
-    // return `This action returns a #${id} transaction`;
     return this.prisma.transaction.findUnique({
       where: { transaction_id: id },
     });
   }
   async findOneDetails(id: number): Promise<Transaction_details[]> {
-    // return `This action returns a #${id} transaction`;
     return this.prisma.transaction_details.findMany({
       where: { transaction_id: id },
     });
@@ -88,10 +77,8 @@ export class TransactionsService {
 
   async update(
     id: number,
-    // updateTransactionDto: Transaction,
     updateTransactionDto: UpdateTransactionDto,
   ): Promise<Transaction> {
-    // return `This action updates a #${id} transaction`;
     return this.prisma.transaction.update({
       where: { transaction_id: id },
       data: updateTransactionDto,
