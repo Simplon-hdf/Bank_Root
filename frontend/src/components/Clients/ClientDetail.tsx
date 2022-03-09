3f8da92c718e39f841bde8c7f07446f4e3b03eb2
DannyPoizot <poizot.dan@gmail.com>
2022-03-09 11:02:22 +0100

    cl tyoe defined

--- frontend/src/components/Clients/ClientDetail.tsx
+++ frontend/src/components/Clients/ClientDetail.tsx
@@ -1,4 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+
+type cl = {
+	client_id:	number;
+    title: string;
+    name: 	string;
+    surname: string;
+    email: string;
+    address: string;
+    telephone: string;
+    login: string;
+    password: string;
+    type: boolean;
+    status_code: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}
 
 // Affichage detail d'un client
 // info, les comptes associés
