e2b12cdbd4693e4891ef8a6c2a5c57d25de0a1f8
DannyPoizot <poizot.dan@gmail.com>
2022-03-10 12:50:26 +0100

    client registration form

--- frontend/src/components/Clients/RegisterForm.tsx
+++ frontend/src/components/Clients/RegisterForm.tsx
@@ -3,18 +3,26 @@ import React, { Component } from 'react';
 class RegisterForm extends Component {
     constructor(props: string) {
         super(props);
-        this.state = {value: ''};
+        this.state = { value: '' };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleChange(e: React.ChangeEvent<HTMLInputElement>) {    
-        this.setState({value: e.target.value});  
+    handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({ value: e.target.value });
     }
 
-    handleSubmit(e: React.MouseEvent<HTMLInputElement>) {    
-       
+    handleSubmit(e: React.MouseEvent<HTMLInputElement>) {
+        const url: string = "http//localhost/5000/clients";
+
+        const dataClients = [];
+
+        //     let getData = () => {
+        //         this.(url)
+        //             .then(res => dataClients.push(res.data))
+        //             .catch(err => console.log(err.data));
+        //     }
     }
 
 
@@ -22,9 +30,18 @@ class RegisterForm extends Component {
 
     render() {
         return (
-            <div>
-                
-            </div>
+            <form>
+                <select>
+                    <option value="Mr">Monsieur</option>
+                    <option value="Mme">Madame</option>
+                </select>
+                <input type="text" placeholder='Nom'></input>
+                <input type="text" placeholder='Prénom'></input>
+                <input type="text" placeholder='Email'></input>
+                <input type="text" placeholder='Adresse'></input>
+                <input type="text" placeholder='Téléphone'></input>
+                <button type="submit">Envoyer</button>
+            </form>
         );
     }
 }
