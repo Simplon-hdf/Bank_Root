81bb39b1385acff4e13f31e152a3c802aedb8472
plx-edu <plx-edu@hotmail.com>
2022-03-11 08:04:17 +0100

    removed usecontext, made base form select choices

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,20 +7,7 @@ import Home from "./components/Home/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Transactions from "./components/Transactions/Transactions";
 
-import {typeAccount} from "./Utilities/types";
-
-// const AppContext = React.createContext<typeAccount[]>([]);
-export const AccountContext = React.createContext<typeAccount[]>([]);
-
 function App() {
-	const [accounts, setAccounts] = useState<typeAccount[]>([]);
-
-	useEffect(() => {
-		fetch(`http://localhost:5000/accounts`)
-			.then((res) => res.json())
-			.then((result) => setAccounts(result));
-	}, []);
-
 	return (
 		<section className="App">
 			<Navbar />
@@ -28,14 +15,7 @@ function App() {
 				<Route path="/" element={<Home />} />
 				<Route path="/clients" element={<Clients />} />
 				<Route path="/accounts" element={<Accounts />} />
-				<Route
-					path="/transactions"
-					element={
-						<AccountContext.Provider value={accounts}>
-							<Transactions />
-						</AccountContext.Provider>
-					}
-				/>
+				<Route path="/transactions" element={<Transactions />} />
 			</Routes>
 		</section>
 	);
--- frontend/src/components/Transactions/TransactionForm.tsx
+++ frontend/src/components/Transactions/TransactionForm.tsx
@@ -1,6 +1,5 @@
 import {get} from "http";
 import React, {useContext, useEffect, useState} from "react";
-import {AccountContext} from "../../App";
 import {getTransacType} from "../../Utilities/methods";
 import {typeAccount} from "../../Utilities/types";
 
@@ -8,17 +7,48 @@ export default function TransactionForm() {
 	const [transacType, setTransacType] = useState(getTransacType()[0]);
 	const [amount, setAmount] = useState(0);
 	const [showFromAcc, setShowFromAcc] = useState(false);
-	const accounts = React.useContext(AccountContext);
-	// const [accounts, setAccounts] = useState<typeAccount[]>([]);
-	const [toAccount, setToAccount] = useState(null);
+	const [accounts, setAccounts] = useState<typeAccount[]>([]);
+	const [toAccount, setToAccount] = useState(-1);
+	const [fromAccount, setFromAccount] = useState(-1);
+	const [initiator, setInitiator] = useState(false);
+
+	useEffect(() => {
+		fetch(`http://localhost:5000/accounts`)
+			.then((res) => res.json())
+			.then((result) => {
+				setAccounts(result);
+				setFromAccount(result[0].account_id);
+				setToAccount(result[0].account_id);
+			});
+	}, []);
 
 	// useEffect(() => {
 	// 	setShowFromAcc(transacType === "Debit" ? true : false);
 	// }, [transacType]);
 
+	function handleSelect() {
+		console.log("handling");
+	}
+
 	function handleSubmit() {
-		if (amount <= 0) return;
-		console.log(amount);
+		if (amount <= 0 || (transacType === "Transfer" && toAccount === fromAccount)) {
+			// console.log("jhbekr");
+			return;
+		}
+		// if(transacType === "Transfer" && toAccount === fromAccount)
+
+		// console.log(":::::::", amount);
+		// console.log(":::::::", fromAccount);
+		// console.log(":::::::", toAccount);
+		const transaction = {
+			from_account_id: transacType === "Transfer" ? fromAccount : null,
+			to_account_id: toAccount,
+			amount: amount,
+			type: transacType,
+			initiated_by: initiator ? -1 : toAccount,
+			status_code: true,
+		};
+		console.log(":::::::", transaction);
 	}
 
 	return (
@@ -37,7 +67,7 @@ export default function TransactionForm() {
 			{transacType === "Transfer" && (
 				<div>
 					<label htmlFor="fromAcc">From: </label>
-					<select name="fromAcc">
+					<select name="fromAcc" onChange={(e) => setFromAccount(+e.target.value)}>
 						{/* <option>From Account</option> */}
 						{accounts.map((acc) => (
 							<option key={acc.account_id} value={acc.account_id}>
@@ -50,7 +80,7 @@ export default function TransactionForm() {
 
 			<div>
 				<label htmlFor="toAcc">{transacType !== "Debit" ? "To:" : "From"} </label>
-				<select name="toAcc">
+				<select name="toAcc" onChange={(e) => setToAccount(+e.target.value)}>
 					{/* <option>To Account</option> */}
 					{accounts.map((acc) => (
 						<option key={acc.account_id} value={acc.account_id}>
@@ -68,8 +98,16 @@ export default function TransactionForm() {
 					min="0"
 					max="10000"
 					step="5"
-					onChange={(e) => setAmount(+e.target.value)}
+					value={amount}
+					onChange={(e) => {
+						setAmount(+e.target.value);
+					}}
 				/>
+			</div>
+
+			<div>
+				<label htmlFor="initiator">Initiated By Bank: </label>
+				<input type="checkbox" onChange={(e) => setInitiator(e.target.checked)} name="initiator" />
 			</div>
 
 			<button type="button" onClick={handleSubmit}>
