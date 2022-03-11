ca0b5b3bd65778aedaf999ca73875abd689fbe44
plx-edu <plx-edu@hotmail.com>
2022-03-11 19:38:18 +0100

    base client fetch for select input

--- frontend/src/components/Accounts/AccountForm.tsx
+++ frontend/src/components/Accounts/AccountForm.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {typeClient} from "../../Utilities/types";
 
 export default function AccountForm() {
-	return <div>AccountForm</div>;
+	const [clients, setClients] = useState<typeClient[]>([]);
+	const [clientId, setClientId] = useState(-1);
+	// const [accountNb, setAccountNb] = useState(-1);
+	// const [balance, setBalance] = useState(0);
+
+	useEffect(() => {
+		fetch(`http://localhost:5000/clients`)
+			.then((res) => res.json())
+			.then((result) => {
+				setClients(result);
+				setClientId(result[0].client_id);
+			});
+	}, []);
+
+	return (
+		<form>
+			<div>
+				<label htmlFor="clientId">Client: </label>
+				<select name="clientId" onChange={(e) => setClientId(+e.target.value)}>
+					{clients.map((cl) => (
+						<option key={cl.client_id} value={cl.client_id}>
+							{/* {`${cl.title}. ${cl.name} (${cl.client_id})`} */}
+							{`${cl.title}. ${cl.name}`}
+						</option>
+					))}
+				</select>
+			</div>
+		</form>
+	);
 }
